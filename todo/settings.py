d46b23c4cbfde47a2db2c5d87434e04fae22d128
Muhammad Kamkoriwala <kamkoriwalamuhammad@gmail.com>
2022-09-12 22:24:12 +0530

    made some changes

--- todo/settings.py
+++ todo/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '$j-)3si!jsb3x8_^vdk7$l!527rl2w60nq(wa)z8u++ao&pk!u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
